ce749e40df3081ffc0dc81edf3fef7a476dc36e5
shrxvx29 <shrxvx29@gmail.com>
2026-09-23 19:42:00 +0530

    server adjusted

--- kickkart-backend/src/main/java/com/kickkart/KickkartBackendApplication.java
+++ kickkart-backend/src/main/java/com/kickkart/KickkartBackendApplication.java
@@ -3,11 +3,15 @@ package com.kickkart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class KickkartBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KickkartBackendApplication.class, args);
+		SpringApplication app = new SpringApplication(KickkartBackendApplication.class);
+		// Railway injects PORT as an environment variable
+		app.setDefaultProperties(Collections.singletonMap("server.port", System.getenv("PORT")));
+		app.run(args);
 	}
-
 }
